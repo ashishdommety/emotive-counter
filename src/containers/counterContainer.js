0651b6c6351dfa101f7e001cc2c63571d3eb3d5a
unknown <ashishuiux@gmail.com>
2018-05-14 15:37:28 -0700

    add note to import actions into CounterContainer.js

--- src/containers/counterContainer.js
+++ src/containers/counterContainer.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Display from '../components/Display';
+import { increment, decrement } from '../actions/index'; // need to figure out how to export the actions in so they can be used here
+
+// console.log(increment);
 
 function mapStateToProps(state){
   return {
