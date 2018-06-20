c0ceffe001639c9063171a4f64a2491854d2f8e1
unknown <ashishuiux@gmail.com>
2018-06-20 09:39:01 -0700

    comment out redux-thunk and use only applyMiddleware to call logger middleware

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { createStore, compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+// import thunk from 'redux-thunk';
 import { Provider} from 'react-redux';
 import counter from './reducers';
 import App from './components/App';
 
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(counter,
-                         compose(
-                           applyMiddleware(thunk),
-                           typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f
-                         )
-                        );
+const logger = (store) => (next) => (action) => {
+  console.log("action fired", action);
+  next(action);
+}
+
+const middleware = applyMiddleware(logger);
+
+const store = createStore(counter, middleware);
 
 ReactDOM.render(
 <Provider store={store}> 
