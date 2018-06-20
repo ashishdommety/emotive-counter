482a6fe0e1af69d3f9ad098cf48c4327238d4890
unknown <ashishuiux@gmail.com>
2018-06-20 09:44:20 -0700

    add comments to explain everything I've done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux'; // import applyMiddleware from redux
 // import thunk from 'redux-thunk';
 import { Provider} from 'react-redux';
 import counter from './reducers';
@@ -9,14 +9,17 @@ import App from './components/App';
 
 import registerServiceWorker from './registerServiceWorker';
 
+/* Create a function that will act as a middleware
+    Here, logger is a function takes the 'store' as an argument, which takes 'next' as an argument, which takes the 'action' as an argument.
+*/
 const logger = (store) => (next) => (action) => {
-  console.log("action fired", action);
-  next(action);
+  console.log("action fired", action); // as soon as the action is dispatched the middleware intercepts it.
+  next(action); // using next(action) allows the action to proceed.
 }
 
-const middleware = applyMiddleware(logger);
+const middleware = applyMiddleware(logger); // create a middleware const that uses applyMiddleware
 
-const store = createStore(counter, middleware);
+const store = createStore(counter, middleware); // add middleware as an argument to the store.
 
 ReactDOM.render(
 <Provider store={store}> 
