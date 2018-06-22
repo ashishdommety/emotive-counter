a9562747708eabc3058a2d1345ded47fa86635bc
unknown <ashishuiux@gmail.com>
2018-06-22 12:12:55 -0700

    add comments

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { createStore, applyMiddleware } from 'redux'; // import applyMiddleware from redux
-// import thunk from 'redux-thunk';
-// import allFuncs from './middleware';
 import axios from 'axios';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
@@ -17,6 +15,7 @@ const middleware = applyMiddleware(logger, thunk); // create a middleware const 
 
 const store = createStore(counter, middleware); // add middleware as an argument to the store.
 
+// dispatch these actions on page load (making async happen with redux)
 store.dispatch((dispatch) =>{
   dispatch({type:'start'});
   axios.get('https://pixabay.com/api/?key=5207088-f6eeef2e4b8e5be697c01704f&q=apples&image_type=photo&pretty=true')
