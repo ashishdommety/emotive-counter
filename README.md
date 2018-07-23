3f28b3f40848f2e4d0ea280d46b072a76ab8a28d
unknown <ashishuiux@gmail.com>
2018-07-23 13:11:52 -0700

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# A repo with redux projects 🚀💻
+# Asynchronous Requests and Middleware in Redux
+
+A simple redux application that makes an ajax request to fetch pictures of apples from pixabay.
+
+Check out my blog on medium called [Diving Deeper into Redux (Async & Middleware)](https://medium.com/@ashcodes/diving-deeper-into-redux-async-middleware-891f12886674).
+
+![App Demo](/readme-assets/diving-deeper-into-redux.gif)
--- src/actions/asyncs.js
+++ src/actions/asyncs.js
@@ -2,10 +2,12 @@ import axios from 'axios'; //  import axios
 
 import * as appleSearch from './index';
 
+const apiKey = 'pixabay api key here';
+
 export const getApples = function getApples(){
   return function(dispatch){
     dispatch(appleSearch.start());
-    axios.get('https://pixabay.com/api/?key=5207088-f6eeef2e4b8e5be697c01704f&q=apples&image_type=photo&pretty=true')
+    axios.get('https://pixabay.com/api/?key='+ apiKey +'&q=apples&image_type=photo&pretty=true')
       .then((response) => {
         
         let images = [];
